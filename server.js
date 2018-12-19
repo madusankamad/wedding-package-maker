6f0a15704bc52e9763b84ca36be90788e9d96b87
udahama <madusanka.dahanyaka@pearson.com>
2018-12-19 16:53:47 +0530

    Adding Test Api

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get("*", (req,res)=>{
     res.sendFile(path.join(__dirname, 'build/index.html'));
 });
+app.get("api/v1", (req,res)=>{
+    res.sendFile(path.join(__dirname, 'build/favicon.ico'));
+});
 
 
 app.listen(port);
