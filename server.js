1a10fb07c5cff837c9527c198c42e6ee896a1adf
udahama <madusanka.dahanyaka@pearson.com>
2018-12-19 20:25:47 +0530

    Adding Backend Proxy

--- server.js
+++ server.js
@@ -5,36 +5,37 @@ const app = express();
 const bodyParser = require('body-parser');
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const router = express.Router();
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+// test route to make sure everything is working (accessed at GET http://localhost:<port>/api)
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'});
 });
-router.get('/magazine', function(req, res) {
-    res.json({ message: 'Magazine Price goes here.....' });
-});
-
-router.get('/enlargements', function(req, res) {
-    res.json({ message: 'Enlargement price goes here....' });
+router.get('/magazine', function (req, res) {
+    res.json({message: 'Magazine Price goes here.....'});
 });
 
+router.get('/enlargements', function (req, res) {
+    res.json({message: 'Enlargement price goes here....'});
+});
 
 
 app.use('/api', router);
 
-/* Static Path */
-app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV !== 'development') {
+    /* Static Path */
+    app.use(express.static(path.join(__dirname, 'build')));
 
-
-/*Routes*/
-app.use("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, 'build/index.html'));
-});
-
+    app.use("*", (req, res) => {
+        res.sendFile(path.join(__dirname, 'build/index.html'));
+    });
+}
+else {
+    console.log('Running in Development mode');
+}
 
 
 app.listen(port);
