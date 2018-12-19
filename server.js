97014bad16b3e473be58e5866d169fc44d10868f
udahama <madusanka.dahanyaka@pearson.com>
2018-12-19 18:33:25 +0530

    Update Server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/api/v1", (req,res)=>{
 });
 /*Routes*/
 app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname, 'build/favicon.ico'));
+    res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 
