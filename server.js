264e42f2147b87112771ab24df93fadc1753f853
udahama <madusanka.dahanyaka@pearson.com>
2018-12-19 17:16:18 +0530

    server test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("*", (req,res)=>{
     res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 app.get("api/v1", (req,res)=>{
-    res.sendFile(path.join(__dirname, 'build/favicon.ico'));
+    res.send({"Hello": "Test"});
 });
 
 
